14da1dacfd3c6e045ea2bb210fc8eba27d3e493e
Matus Kundrat <matus.kundrat@globallogic.com>
2023-05-23 23:48:13 +0200

    Added more tests for search repo

--- tests/api/test_github_search_repo.py
+++ tests/api/test_github_search_repo.py
@@ -10,7 +10,7 @@ def test_search_repo_positive(git_hub_api_client):
     repos_list = git_hub_api_client.search_repo(repo_name)
 
     # Validate the search returns results
-    assert len(repos_list) != 0
+    assert repo_name in repos_list
 
 def test_search_repo_negative(git_hub_api_client):
     """
@@ -24,4 +24,19 @@ def test_search_repo_negative(git_hub_api_client):
     repos_list = git_hub_api_client.search_repo(repo_name)
 
     # Validate the search returns results
-    assert len(repos_list) == 0
+    assert repo_name not in repos_list
+
+def test_search_repo_millions_result(git_hub_api_client):
+    """
+    This test search for existing repo on Github
+    """
+
+    # prepare the existing repo name
+    repo_name = 'a'
+    per_page = 100
+
+    # search for the repo from step #1
+    repos_list = git_hub_api_client.search_repo(repo_name, per_page)
+
+    # Validate the search returns results
+    assert len(repos_list) == per_page
