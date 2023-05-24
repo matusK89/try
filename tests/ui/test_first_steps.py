31314dcfc5fc18e7bbb3c4cebaf31ad1ee570929
Matus Kundrat <matus.kundrat@globallogic.com>
2023-05-24 10:48:33 +0200

    added test_login_negative

--- tests/ui/test_first_steps.py
+++ tests/ui/test_first_steps.py
@@ -1,5 +1,28 @@
-def test_login(user_fixture):
-    print("test login")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_login_negative():
+    # create driver
+    driver = webdriver.Chrome()
+
+    # open login page
+    driver.get("https://github.com/login")
+
+    # enter wrong/correct email
+    login_field = driver.find_element(By.ID, "login_field")
+    login_field.send_keys("some_incorrect_email@gmail.com")
+
+    # enter wrong password
+    pass_field = driver.find_element(By.ID, "password")
+    pass_field.send_keys("dgkhdkkjfkj")
+
+    # click sign in button
+    singin_btn = driver.find_element(By.NAME, "commit")
+    singin_btn.click()
+
+    # check error message
     
-    assert 2 == 1
-    
+
+    driver.quit()
